2c5ea9ee486bf5155719c44134c52d55aeeee1af
p2201085 <gatta.code@gmail.com>
2023-07-13 22:55:19 +0200

    Added Edit and Remove todo

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,10 @@ const addTodo = () => {
   })
 }
 
+const removeTodo = todo => {
+  todos.value = todos.value.filter(t => t !== todo)
+}
+
 watch(todos, (newVal) => {
   localStorage.setItem('todos', JSON.stringify(newVal))
 }, { deep: true })
@@ -76,7 +80,19 @@ onMounted(() => {
     <section class="todo-list">
       <h3>TODO LIST</h3>
       <div class="list">
-        <div v-for="todo in todos_asc" :class="todo-item ${todo.done}"></div>
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+          <input type="checkbox" v-model="todo.done">
+          <span :class="`bubble ${todo.category}`"></span>
+        </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
       </div>
     </section>
   </main>
